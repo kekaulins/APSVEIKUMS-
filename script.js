106af409c159c415479cb892338a742c76519c3f
kekaulins <christianskaulins@gmail.com>
2024-12-11 10:24:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const poga = document.querySelector("button");
+
+poga.addEventListener("mouseover", () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
